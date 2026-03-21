560f5a1a5a9604f13aa6b2f7baebe5c34a85518c
李勇 <2236472089@qq.com>
2026-03-21 20:26:48 +0800

    style: 调整 CSS 重置顺序并引入 TDesign Mobile Vue 组件库
    调整 reset.css 中 margin 和 padding 的声明顺序以保持一致性。
    引入 tdesign-mobile-vue 组件库及其样式，并添加 @iconify/vue 以在布局中使用图标。

--- src/layouts/index.vue
+++ src/layouts/index.vue
@@ -2,12 +2,18 @@
 import {
   RouterView
 } from "vue-router";
+
+import {
+  Icon
+} from "@iconify/vue";
+
 </script>
 <template>
   <main>
     <header>头部</header>
     <content>
       <RouterView />
+      <Icon icon="mdi-light:home" />
     </content>
     <footer>底部</footer>
   </main>
