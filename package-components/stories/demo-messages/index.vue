d080d9f85936d30f4fe6d8b2526a6027334731f8
李勇 <2236472089@qq.com>
2026-03-22 03:22:13 +0800

    feat(messages): 添加消息组件并导出 chore(tsconfig): 调整 tsconfig 引用顺序并更新配置 fix(tsconfig.node): 移除不必要的 verbatimModuleSyntax 配置

---
+++ package-components/stories/demo-messages/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import {
+  Button,
+  Message
+} from "tdesign-mobile-vue";
+
+const handleClick = () => {
+  Message.error({
+    content: "这是一个消息提示",
+    options: {
+      duration: 3000
+    }
+  });
+};
+</script>
+<template>
+  <div>
+    <Button @click="handleClick">
+      按钮
+    </Button>
+  </div>
+</template>
+<style scoped>
+div {
+  font-size: 16px;
+}
+</style>
